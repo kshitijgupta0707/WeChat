6598647196002d14800be83eecfa5d8ab213ad38
Kshitij gupta <kshitijgupta070704@gmail.com>
2025-01-03 18:13:26 +0530

    Chat with ai done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -287,8 +287,9 @@ export const logout = (req, res) => {
         if (!allowedTypes.includes(profilePic.mimetype)) {
           return res.status(400).json({ message: "Only JPG and PNG files are allowed" });
         }
-        if (profilePic.size > 5 * 1024 * 1024) {
-          return res.status(400).json({ message: "File size must be less than 5MB" });
+        // only 5 mb photo rquired
+        if (profilePic.size > 1 * 1024 * 1024) {
+          return res.status(400).json({ message: "File size must be less than 1MB" });
         }
   
         const uploadResponse = await uploadImageToCloudinary(profilePic, "CHATAPP");
