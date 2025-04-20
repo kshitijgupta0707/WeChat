d03b85414e149760ef7a5c2ee8c2d69e0c7c242a
= <kshitijgupta070704@gmail.com>
2025-04-20 21:13:43 +0530

    change the name

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
                 </div>
 
               </button>
-                <h1 className=" text-lg font-bold">WeChat</h1>
+                <h1 className=" text-lg font-bold">BaatCheet</h1>
               </Link>
              
 
