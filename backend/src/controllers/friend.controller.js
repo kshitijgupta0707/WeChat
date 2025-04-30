4a5e43f024b0ea4c9e0e5f877214254c43ab53f4
= <kshitijgupta070704@gmail.com>
2025-04-30 12:23:37 +0530

    notification are working fine

--- backend/src/controllers/friend.controller.js
+++ backend/src/controllers/friend.controller.js
@@ -330,38 +330,27 @@ export const acceptFriendRequest = async (req, res) => {
       });
       console.log("Notified the front end about the friend accept");
     }
-
-
-
-
-
     try {
       console.log("send Freind request contolelr is is callled");
       console.log("Notifyig that person");
       const notifyPerson = await sendNotificationToPerson(
-        `New Request!`,
-        `You have recieved a friend Request from  ${req.user.firstName}.`,
+        `Friend Request Accepted!`,
+        `Your friend Request to  ${req.user.firstName} has been accepted.`,
         { userId: friendId, type: "new Request recieved" }
       );
 
       // Send notification to all users - using the updated function with lock mechanism
-      const notificationResult = await sendNotificationToAll(
-        `New Request !`,
-        `You have recieved aFriend Request from  ${req.user.firstName}.`,
-        { type: "new Request recieved" }
-      );
+      // const notificationResult = await sendNotificationToAll(
+      //   `New Request !`,
+      //   `You have recieved aFriend Request from  ${req.user.firstName}.`,
+      //   { type: "new Request recieved" }
+      // );
 
-      console.log("Notification result:", notificationResult);
+      console.log("Notification result:", notifyPerson);
     } catch (notificationError) {
       // Log the error but don't fail the entire request
       console.error("Failed to send notifications:", notificationError);
     }
-
-
-
-
-
-
     return res.status(200).json({
       success: true,
       message: "Friend request accepted successfully",
@@ -414,18 +403,18 @@ export const declineFriendRequest = async (req, res) => {
       console.log("Notifyig that person");
       const notifyPerson = await sendNotificationToPerson(
         `New Request!`,
-        `You have recieved a friend Request from  ${req.user.firstName}.`,
+        `${req.user.firstName} have declined your friend request.`,
         { userId: friendId, type: "new Request recieved" }
       );
 
-      // Send notification to all users - using the updated function with lock mechanism
-      const notificationResult = await sendNotificationToAll(
-        `New Request !`,
-        `You have recieved aFriend Request from  ${req.user.firstName}.`,
-        { type: "new Request recieved" }
-      );
+      // // Send notification to all users - using the updated function with lock mechanism
+      // const notificationResult = await sendNotificationToAll(
+      //   `New Request !`,
+      //   `You have recieved aFriend Request from  ${req.user.firstName}.`,
+      //   { type: "new Request recieved" }
+      // );
 
-      console.log("Notification result:", notificationResult);
+      console.log("Notification result:", notifyPerson);
     } catch (notificationError) {
       // Log the error but don't fail the entire request
       console.error("Failed to send notifications:", notificationError);
